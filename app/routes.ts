9e955001c0becf44fb16b243e3cc6e17ff4c944f
Joseph <JosephVolpe@outlook.com>
2022-09-16 08:44:51 -0400

    Routes added for restapi

--- app/routes.ts
+++ app/routes.ts
@@ -11,22 +11,43 @@ export const Routes = [{
     route: "/users",
     controller: UserController,
     action: "all",
-}, {
-    method: "get",
-    route: "/users/:id",
-    controller: UserController,
-    action: "one",
-}, {
+},
+{
     method: "post",
     route: "/users",
     controller: UserController,
     action: "save",
-}, {
+},
+{
+    method: "put",
+    route: "/users/:id",
+    controller: UserController,
+    action: "update",
+},
+{
+    method: "get",
+    route: "/users/:id",
+    controller: UserController,
+    action: "one",
+},
+{
+    method: "post",
+    route: "/users/login",
+    controller: UserController,
+    action: "findByEmail",
+    },
+{
+    method: "post",
+    route: "/users/register",
+    controller: UserController,
+    action: "checkExistingAccount",
+},
+{
     method: "delete",
     route: "/users/:id",
     controller: UserController,
     action: "remove",
-    },
+},
 
 
     // Book Routes
@@ -35,24 +56,44 @@ export const Routes = [{
     route: "/books",
     controller: BookController,
     action: "all",
-    },
+},
 {
     method: "get",
     route: "/books/:id",
     controller: BookController,
     action: "one",
-    }, 
+}, 
 {
     method: "post",
     route: "/books",
     controller: BookController,
     action: "save",
-    }, 
+}, 
+{
+    method: "put",
+    route: "/books/:id",
+    controller: BookController,
+    action: "update",
+},
 {
     method: "delete",
     route: "/books/:id",
     controller: BookController,
     action: "remove",
+    }, 
+
+{
+    method: "get",
+    route: "/books/search/:id",
+    controller: BookController,
+    action: "findByIdLessThan",
+    }, 
+    
+{
+    method: "get",
+    route: "/books/search/author/:author",
+    controller: BookController,
+    action: "findByAuthor",
     }, 
 
     //Chapter Routes
@@ -64,7 +105,13 @@ export const Routes = [{
     }, 
 {
     method: "get",
-    route: "/chapters/:id",
+    route: "/chapters/:book_id",
+    controller: ChapterController,
+    action: "findByBook",
+    }, 
+{
+    method: "get",
+    route: "/chapter/:id",
     controller: ChapterController,
     action: "one",
     }, 
@@ -73,6 +120,12 @@ export const Routes = [{
     route: "/chapters",
     controller: ChapterController,
     action: "save",
+    }, 
+{
+    method: "put",
+    route: "/chapter/:id",
+    controller: ChapterController,
+    action: "update",
     }, 
 {
     method: "delete",
@@ -91,7 +144,7 @@ export const Routes = [{
     }, 
 {
     method: "get",
-    route: "/libraries/:id",
+    route: "/library/:id",
     controller: LibraryController,
     action: "one",
     }, 
@@ -106,5 +159,25 @@ export const Routes = [{
     route: "/libraries/:id",
     controller: LibraryController,
     action: "remove",
+    }, 
+
+{
+    method: "get",
+    route: "/libraries/:id",
+    controller: LibraryController,
+    action: "findByUser",
+    }, 
+
+{
+    method: "get",
+    route: "/libraries/:userid/:bookId",
+    controller: LibraryController,
+    action: "findBookByUser",
+    }, 
+{
+    method: "post",
+    route: "/libraries/:bookId/:userId",
+    controller: LibraryController,
+    action: "bookmark",
     }, 
 ]
